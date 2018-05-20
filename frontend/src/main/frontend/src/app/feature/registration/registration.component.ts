996944d6b93b931e0b9c4d14dac72f209eea73a6
Richard Nduka <koolrich@gmail.com>
2018-05-20 22:48:31 +0100

    Registration - part 1

--- frontend/src/main/frontend/src/app/feature/registration/registration.component.ts
+++ frontend/src/main/frontend/src/app/feature/registration/registration.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators} from '@angular/forms';
+
+import { Settings } from '../../app.settings.model';
+import { AppSettings } from '../../app.settings';
+import { emailValidator, matchingPasswords } from '../shared/util/validators';
 
 @Component({
   selector: 'app-registration',
@@ -6,10 +11,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration.component.scss']
 })
 export class RegistrationComponent implements OnInit {
+  public settings: Settings;
+  public form: FormGroup;
 
-  constructor() { }
+  constructor(public appSettings: AppSettings, public fb: FormBuilder) {
+    this.settings = this.appSettings.settings;
+    this.form = this.fb.group({
+      'name': [null, Validators.compose([Validators.required, Validators.minLength(3)])],
+      'email': [null, Validators.compose([Validators.required, emailValidator])],
+      'password': ['', Validators.required],
+      'confirmPassword': ['', Validators.required]
+    },{validator: matchingPasswords('password', 'confirmPassword')});
+  }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    setTimeout(() => { this.settings.loadingSpinner = false; }, 300);
   }
 
 }
